0892bc9ccef6831518abf9bf707a73fa2be88012
dineshdinu21 <dineshmurugan654@gmail.com>
2026-05-06 20:04:52 +0530

    PostgreSQL Connected

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,10 +55,10 @@ WSGI_APPLICATION = 'my_shop.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3', # Laptop-la irukkumbothu sqlite use pannum
+        conn_max_age=600
+    )
 }
 
 
